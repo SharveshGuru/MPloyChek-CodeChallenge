219e3711bb346edb75795091775021060e92808a
Sharvesh <sharveshguru2004@gmail.com>
2025-03-25 14:25:05 +0530

    creation of user model and record model

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['Admin', 'General User'], default: 'General User' }
+});
+
+UserSchema.post('save', function(error, doc, next) {
+    if (error.name === 'MongoServerError' && error.code === 11000) {
+        next(new Error('Email already exists'));
+    } else {
+        next(error);
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
