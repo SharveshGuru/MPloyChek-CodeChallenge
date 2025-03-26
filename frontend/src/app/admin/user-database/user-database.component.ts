bba2dd87f65a1b6cd126de4bac84fb4bc487ad83
Sharvesh <sharveshguru2004@gmail.com>
2025-03-26 12:33:51 +0530

    initialized user database component

---
+++ frontend/src/app/admin/user-database/user-database.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-database',
+  imports: [],
+  templateUrl: './user-database.component.html',
+  styleUrl: './user-database.component.css'
+})
+export class UserDatabaseComponent {
+
+}
